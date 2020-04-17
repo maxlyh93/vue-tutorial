2fab4772b9717b663f8924cead2790a229b9fd0a
maxlyh93 <maxlyh93@gmail.com>
2020-04-17 19:31:09 +0800

    CRUD functionality without apis

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,17 +2,24 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table v-bind:employees="employees"/>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table 
+    :employees="employees" 
+    @delete:employee="deleteEmployee" 
+    @edit:employee="editEmployee"
+    />
   </div>
 </template>
 
 <script>
   import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
 
   export default {
     name: 'app',
     components: {
       EmployeeTable,
+      EmployeeForm,
     },
     data() {
     return {
@@ -35,6 +42,28 @@
       ],
     }
   },
+  methods: {
+  addEmployee(employee) {
+    const lastId =
+    this.employees.length > 0
+      ? this.employees[this.employees.length - 1].id
+      : 0;
+    const id = lastId + 1;
+    const newEmployee = { ...employee, id };
+
+    this.employees = [...this.employees, newEmployee];
+    },
+    deleteEmployee(id) {
+    this.employees = this.employees.filter(
+      employee => employee.id !== id
+    )
+  },
+  editEmployee(id, updatedEmployee) {
+  this.employees = this.employees.map(employee =>
+    employee.id === id ? updatedEmployee : employee
+  )
+}
+  }
 }
 </script>
 
